7eed61ec626e61ae0aedc330715d6176dcecab4b
Katy Huff <katyhuff@gmail.com>
2014-04-03 16:04:19 -0700

    fixed mailing to be what we sent this year.

--- mailings/program_committee/send-decisions.py
+++ mailings/program_committee/send-decisions.py
@@ -13,106 +13,65 @@ from email.mime.multipart import MIMEMultipart
 from email.mime.text import MIMEText
 from email.mime.base import MIMEBase
 
-if len(sys.argv) < 5:
-    print('Usage: ./send-decisions.py decisions.csv template.txt.in title_authors.csv author_comments.csv')
-
-decisions = np.loadtxt(sys.argv[1],
-        delimiter='\t',
-        dtype={'names': ('id', 'decision'),
-            'formats': ('S32', 'S32')})
-
-def decision_to_poster_talk(decision):
-    if decision.strip() == 'poster':
-        return 'poster'
-    return 'talk'
+if len(sys.argv) < 3:
+    print('Usage: ./send-invitations.py pc_invitees.csv pc_invitation.txt.in')
 
 with open(sys.argv[2], 'r') as fp:
     email_template = fp.read()
 template = jinja2.Template(email_template)
 
-title_authors = np.loadtxt(sys.argv[3],
-        delimiter='\t',
-        dtype={'names': ('id', 'title', 'authors', 'emails'),
-            'formats': ('S32', 'S1024', 'S512', 'S128')})
+pc_invitees = np.loadtxt(sys.argv[1],
+        delimiter=',',
+        dtype={'names': ('invitee', 'email','fields'),
+            'formats': ('S128', 'S128', 'S128')})
 
-track_mini_name_map = {'gen': 'General Track',
-        'tfr': 'Reproducibility Track',
-        'ml': 'Machine Learning Track',
-        'gis': 'GIS Mini-symposium',
-        'met': 'Meteorology, Climatology, and Atmospheric and Oceanic Science Mini-symposium',
-        'ast': 'Astronomy and Astrophysics Mini-symposium',
-        'img': 'Medical Imaging Mini-symposium',
-        'bio-f': 'Bioinformatics Mini-symposium',
-        'bio-s': 'Bioinformatics Mini-symposium',
-        'bio-m': 'Bioinformatics Mini-symposium',
-        'poster': 'Poster Session'}
+expertise_name_map = {
+        'cs': 'scientific computing with python',
+        'edu': 'scientific computing education',
+        'geo': 'geophysics',
+        'gis': 'geospatial data',
+        'gissci': 'geospatial data in science',
+        'astro': 'astronomy and astrophysics',
+        'viz': 'visualization',
+        'soc': 'computational social sciences',
+        'bioinfo': 'bioinformatics',
+        'eng': 'engineering'}
 
-def decision_is_poster(decision):
-    if decision.strip() == 'poster':
-        return True
-    return False
-
-with open(sys.argv[4], 'r') as fp:
-    author_comments = []
-    lines = fp.readlines()
-    for line in lines:
-        author_comments.append(line.split('\t'))
-
-def get_standard_id(_id):
-    return _id.lower().strip()
-
-def get_author_comments(_id, author_comments):
-    id_ = get_standard_id(_id)
-    comments = []
-    for review in author_comments:
-        if get_standard_id(review[0]) == id_:
-            comments.append(review[1])
-    if len(comments) == 0:
-        comments.append('No comments')
-
-    return comments
-
-def get_decision(_id, decisions):
-    _id = get_standard_id(_id)
-    for decision in decisions:
-        if get_standard_id(decision['id']) == _id:
-            return decision['decision']
-
-    raise IndexError('Could not find id: ' + _id + ' decision')
-
-
-username = 'matthew.m.mccormick@gmail.com'
+def get_fields(fields):
+    split_fields = fields.split(" ")
+    if len(split_fields) == 1:
+        fieldstr = expertise_name_map[split_fields[0]]
+        is_are_str = "is"
+    if len(split_fields) == 2:
+        fieldstr = expertise_name_map[split_fields[0]] +\
+        " and " +\
+        expertise_name_map[split_fields[1]]
+        is_are_str = "are"
+    if len(split_fields) > 2:
+        raise Exception("Too many fields in"+fields)
+    return fieldstr, is_are_str
+    
+username = 'katyhuff@gmail.com'
 password = getpass.getpass('password:')
 server = smtplib.SMTP('smtp.gmail.com:587')
 server.starttls()
 server.login(username, password)
-for submission in title_authors:
-    decision = get_decision(submission['id'], decisions)
-    authors = submission['authors'].split(';')
-    submitters = ''
-    for author in authors:
-        name_split = author.split(',')
-        if len(name_split) == 1:
-            submitters += name_split[0] + ', '
-        else:
-            submitters += name_split[1] + ' ' + name_split[0] + ', '
 
-    email_body = template.render(submitters=submitters,
-            abstract_title=submission['title'],
-            poster_talk=decision_to_poster_talk(decision),
-            track_mini_name=track_mini_name_map[decision],
-            is_poster=decision_is_poster(decision),
-            reviewers_comments=get_author_comments(submission['id'], author_comments))
+for member in pc_invitees:
+    fieldstr, is_are_str =get_fields(member['fields'])
+    email_body = template.render(name=member['invitee'], 
+        expertise=fieldstr,
+        isare=is_are_str) 
     msg = MIMEMultipart('alternative')
-    msg['Subject'] = 'Abstracts to review for SciPy 2013'
-    msg['From'] = 'Matthew McCormick <matthew.m.mccormick@gmail.com>'
-    msg['To'] = submission['emails']
-    msg['Cc'] = 'Katy Huff <katyhuff@gmail.com>, Andy Terrel <andy.terrel@gmail.com>'
+    msg['Subject'] = 'Invitation to SciPy2014 Program Committee'
+    msg['From'] = 'Katy Huff <katyhuff@gmail.com>'
+    msg['To'] = member['email']
+    msg['Cc'] = 'Serge Rey <sjsrey@gmail.com>,'
     msg['Date'] = email.utils.formatdate()
     msg.attach(MIMEText(email_body, 'plain'))
-    from_address = 'Matthew McCormick <matthew.m.mccormick@gmail.com>'
-    to_address = ['Katy Huff <katyhuff@gmail.com>', 'Andy Terrel <andy.terrel@gmail.com>']
-    to_address.extend([em.strip() for em in submission['emails'].split(',')])
+    from_address = 'Katy Huff <katyhuff@gmail.com>'
+    to_address = ['Serge Rey <sjsrey@gmail.com>']
+    to_address.extend([em.strip() for em in member['email'].split(',')])
 
     print(email_body)
     server.sendmail(from_address, to_address, msg.as_string())
